0aff5b78ea8f982243c2af1e8f744df55e7fc0f0
bestdevmgp <me@mingyu.dev>
2025-11-21 17:18:55 +0900

    chore: modify baseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,7 +10,7 @@ import type {
 } from '../types';
 import {env} from "@headlessui/react/dist/utils/env";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://api.share.mingyu.dev';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
