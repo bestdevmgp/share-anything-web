da03e4b88fcc103110e76380044ad81b2cc26eb6
bestdevmgp <me@mingyu.dev>
2026-02-04 02:43:28 +0900

    style: toast

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -30,7 +30,7 @@ const ToastIcon: React.FC<{ type: ToastType['type'] }> = ({ type }) => {
     info: {
       bgColor: 'bg-blue-500',
       icon: (
-        <svg className="w-5.5 h-5.5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M12 16v-4m0-4h.01" />
         </svg>
       ),
