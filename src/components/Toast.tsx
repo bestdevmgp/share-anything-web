78dd233f8c39a32412c65ecd2702fd0f261de16a
bestdevmgp <me@mingyu.dev>
2026-02-05 01:42:35 +0900

    chore: toast

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -22,8 +22,8 @@ const ToastIcon: React.FC<{ type: ToastType['type'] }> = ({ type }) => {
     warning: {
       bgColor: 'bg-amber-500',
       icon: (
-        <svg className="w-3.5 h-3.5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M12 9v2m0 4h.01" />
+        <svg className="w-5.5 h-5.5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 16v-4m0-4h.01" />
         </svg>
       ),
     },
