fed27fc4a37f2dd3b4a0af11092e0e1ca7288236
bestdevmgp <me@mingyu.dev>
2026-02-12 01:21:43 +0900

    fix: upload history layout

--- src/pages/UploadHistoryPage.tsx
+++ src/pages/UploadHistoryPage.tsx
@@ -328,11 +328,11 @@ const UploadHistoryPage: React.FC = () => {
               <table className="w-full min-w-[1200px] divide-y divide-gray-200 dark:divide-white/10 table-fixed">
                 <colgroup>
                   <col style={{ width: '22%' }} />
-                  <col style={{ width: '12%' }} />
-                  <col style={{ width: '14%' }} />
-                  <col style={{ width: '14%' }} />
-                  <col style={{ width: '12%' }} />
-                  <col style={{ width: '12%' }} />
+                  <col style={{ width: '10%' }} />
+                  <col style={{ width: '18%' }} />
+                  <col style={{ width: '18%' }} />
+                  <col style={{ width: '9%' }} />
+                  <col style={{ width: '9%' }} />
                   <col style={{ width: '14%' }} />
                 </colgroup>
                 <thead className="bg-gray-50 dark:bg-white/5">
@@ -406,7 +406,7 @@ const UploadHistoryPage: React.FC = () => {
                           )}
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
-                          <div className="flex justify-end gap-0.5">
+                          <div className="flex justify-center gap-0.5">
                             {!isExpired(upload.expires_at) && (
                               <button
                                 onClick={(e) => handleShowQRCode(upload.share_code, e)}
