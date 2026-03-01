6e31d4276916fd3fe5b70201781ddc842e6e576b
bestdevmgp <me@mingyu.dev>
2026-03-02 08:11:00 +0900

    chore: magic-link login

--- src/pages/EmailVerifyWaitPage.tsx
+++ src/pages/EmailVerifyWaitPage.tsx
@@ -198,7 +198,7 @@ const EmailVerifyWaitPage: React.FC = () => {
 
             <div className="flex justify-center mb-5">
               <div className="w-16 h-16 rounded-full bg-primary/10 flex items-center justify-center">
-                <EnvelopeIcon className="w-10 h-10 text-primary" strokeWidth={2.5} />
+                <EnvelopeIcon className="w-10 h-10 text-primary" strokeWidth={2} />
               </div>
             </div>
 
